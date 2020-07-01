61c9319cdc12a83b51034cb1fec2753f85b45dc4
vmalabanan <63671843+vmalabanan@users.noreply.github.com>
2020-06-30 20:11:41 -0700

    Create mathematical.py

---
+++ Module 4 Homework/mathematical.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    number_underscore = 25_000_000
+    number = 25000000
+    print(f'number_underscore: {number_underscore}')
+    print(f'number: {number}')
+
+    e_var = 1.75e5
+    print(e_var)
+
+    base = input('Enter a base: ')
+    exponent = input('Enter an exponent: ')
+    value = int(base) ** int(exponent)
+    print(f'{base} to the power of {exponent} = {value}')
+    
